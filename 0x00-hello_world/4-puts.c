5e49d17b85db6bc81442c0c1be05d78a188bb23c
Abel <abeldawit.h@gmail.com>
2022-12-08 09:39:49 -0800

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * This is realy fun
  * i'm enjoying 
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle" );
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
