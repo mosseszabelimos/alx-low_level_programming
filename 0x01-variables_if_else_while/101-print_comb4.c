ab62733e764ca19ff6a5b8d1089ffb9589702ab0
Abel <abeldawit.h@gmail.com>
2022-12-09 14:32:16 -0800

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,17 +16,19 @@ int main(void)
 		for (num2 = '0'; num2 <= '9'; num2++)
 		{
 			for (num1 = '0'; num1 <= '9'; num1++)
-				if (!((num1 == num2) || (num2 == num1) || (num2 > num1) || (num3 > num2)))
+			{
+				if (!((num1 == num2) || (num2 == num3) || (num2 > num1) || (num3 > num2)))
 				{	
 					putchar(num3);
 					putchar(num2);
 					putchar(num1);
-					if (!(num1 == '9' && num3 =='7' && num2 == '8'))
+					if (!(num1 == '9' && num3 == '7' && num2 == '8'))
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
+			}
 		}
 	}		
 	putchar('\n');
