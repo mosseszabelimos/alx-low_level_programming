0fb61b7602c1de506c23cee1c2a7570a5543103c
Abel <abeldawit.h@gmail.com>
2022-12-09 10:55:21 -0800

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void) 
+{
+	char cha1;
+
+	cha1 = 'z';
+	while 
+		(cha1 >= 'a') {
+			putchar(cha1);
+			cha1--;
+		}
+	putchar('\n');
+	return (0);
+}
+
