c4b9135a7e245c710ebaf97dac521a5b923aa206
Abel <abeldawit.h@gmail.com>
2022-12-09 09:55:19 -0800

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch1;
+
+	char ch2;
+
+	ch1 = 'a';
+	ch2 = 'A';
+	while
+		(ch1 <= 'z'){
+			putchar(ch1);
+			ch1++;
+		}
+	while
+		(ch2 <= 'Z'){
+			putchar(ch2);
+			ch2++;
+		}
+	return (0);
+}
