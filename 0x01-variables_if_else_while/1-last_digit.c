6dd6f5de441e559c75472e623dbaeba4d923e19a
Abel <abeldawit.h@gmail.com>
2022-12-09 09:14:58 -0800

    1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,10 +5,10 @@
 
 /* betty style doc for function main goes there */
 /**
-* main -Entry point
-* 
-* Return: Always 0 (sucess)
-*/
+ * main -Entry point
+ * 
+ * Return: Always 0 (sucess)
+ */
 int main(void)
 {
 	int n;
