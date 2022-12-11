92b376c44d92729385247038f1de17d724d92e90
Abel <abeldawit.h@gmail.com>
2022-12-11 06:38:33 -0800

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ int main (void)
 				putchar(' ');
 				putchar(( num2 / 10) +48);
 				putchar((num2 % 10) +48);
-				if (num1 != 98 || num2 !=98)
+				if (num1 != 98 || num2 !=99)
 				{
 					putchar(',');
 					putchar(' ');
