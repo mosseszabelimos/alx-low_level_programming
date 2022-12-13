eef8c19cb7d0bcd0187c2e064946b55f55e82afc
Abel <abeldawit.h@gmail.com>
2022-12-13 08:15:40 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10(void) : Entry Point
+ * Return value if there is
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i =0; i<=9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			{
+				_putchar(ch);
+			}
+	}
+	
+	_putchar('\n');
+}
+
+
