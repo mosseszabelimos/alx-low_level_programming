94bc03e289b5454c580d09f84ac6f50057ae1182
Abel <abeldawit.h@gmail.com>
2022-12-13 08:18:30 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	for (i =0; i<=9; i++)
+	for (i = 0; i<= 9; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
-			{
-				_putchar(ch);
-			}
+		{
+			_putchar(ch);
+		}
 		_putchar('\n');
 	}
 }
