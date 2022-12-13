a8bf249c96d1f77865c551bc84bc6f4af9309bdf
Abel <abeldawit.h@gmail.com>
2022-12-13 09:10:54 -0800

    5. sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n; is the number to be determined
+ * Returns: 1 if +, 0 if zero, -1 if negative;
+ */
+
+int print_sign(int)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} 
+	else if ( n == 0)
+	{
+		_putchar('0')
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-1');
+		return (-1);
+	}
+}
