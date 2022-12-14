8ce3707189ff149352b3ba72b8615b5f841cfb02
Abel <abeldawit.h@gmail.com>
2022-12-14 05:22:44 -0800

    Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,9 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-void print_times_table(int n);
 void positive_or_negative(int i);
-void largest_number(int a, int b, int c);
-void print_remainindays(int month, int day, int year);
-int convert_day(int month, int day);
+
 #endif
 
