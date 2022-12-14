70b104f2ba78e0f9e553c18fd95066fb7ecf6287
Abel <abeldawit.h@gmail.com>
2022-12-14 00:56:05 -0800

    14.computer class

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main- Prints first 50 fibonachi
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	unsigned long num1, num2, sum;
+	
+	num1 = 0;
+	num2 = 1;
+	
+	for (x = 0; x < 50; x++)
+	{
+		sum = num1 + num2;
+		printf("%lu", sum);
+		num1 = num2;
+		num2 = sum;
+
+		if (x == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
+	
