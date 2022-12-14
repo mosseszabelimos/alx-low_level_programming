359aa177a736600db68d8829fc2c643d39547849
Abel <abeldawit.h@gmail.com>
2022-12-14 00:40:37 -0800

    12. The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -38,7 +38,7 @@ void print_times_table(int num)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((z / 100) + '0');
-					_putchar(((z /10) % 10) + '0');
+					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
 				}
 			}
