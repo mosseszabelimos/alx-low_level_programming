86722c33041d8d53a9d8dce2787ae1e889a7557b
Abel <abeldawit.h@gmail.com>
2022-12-15 08:09:56 -0800

    0.isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if letter is upper
+ * @i: to be checked
+ *
+ * Return: 1 for the upper
+ */
+
+int _isupper(int i)
+{
+	if (i >= 65 && i <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+int _isupper(int c);
+void print_numbers(void);
+
+#endif
