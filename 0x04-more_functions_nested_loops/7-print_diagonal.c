739c3648a0ea4501acd9fd7c41affa6930091913
Abel <abeldawit.h@gmail.com>
2022-12-15 10:30:41 -0800

    7. i feel like...

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,24 @@
-#include::wq
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal
+ * @n: diagonal
+ * Return: no return
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+	
+	for ( i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
