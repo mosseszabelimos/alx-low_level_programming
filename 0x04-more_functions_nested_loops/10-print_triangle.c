11d1e738e8c0f1d661243f15807789673c68ef8a
Abel <abeldawit.h@gmail.com>
2022-12-16 00:23:53 -0800

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,14 +18,14 @@ void print_triangle(int size)
 	{
 		for (x = 0; x < size; x++)
 		{
-		for (y = size -x; y > 1; y--)
-		{	
-			_putchar(32);
-		}
-		for (z = 0; z <=nx; z++)
-		{
-			_putchar(35);
-		}
+			for (y = size - x; y > 1; y--)
+			{	
+				_putchar(32);
+			}
+			for (z = 0; z <=nx; z++)
+			{
+				_putchar(35);
+			}
 			_putchar('\n');
 		}
 	}
