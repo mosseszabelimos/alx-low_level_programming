7bd0d8ebfdea51f33e02c3fd0b490589297eb617
Abel <abeldawit.h@gmail.com>
2022-12-19 22:06:48 +0300

    9.strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed by scr
+ * @gate1: first variable
+ * @gate2: copy
+ * Return: return value
+ */
+
+char *_strcpy(*char gate1, char gate2)
+{
+	char *s = gate1;
+
+	while (*gate2 != '\0')
+	{
+		*gate1 = *gate2;
+		gate1++;
+		gate2++;
+	}
+	*gate1 = '\0';
+	return (s);
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,4 +11,5 @@ void rev_string(char *ch);
 void puts2(char *ch);
 void puts_half(char *ch);
 void print_array(int *x, int num);
+char *_strcpy(char *ch1, char *ch2);
 #endif
