2bef4340af7c3aaac206382423bc973964819870
Abel <abeldawit.h@gmail.com>
2022-12-21 07:46:54 -0800

    5. Always Look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all letters of string to upper case
+ * @x: entry;
+ * Return: value
+ */
+
+char *string_toupper(char *x)
+{
+	int y;
+
+	y = 0;
+	while (x[y] != '\0')
+	{
+		if (x[y] >= 'a' && x[y] <= 'Z')
+			x[y] = x[y] - 32;
+		y++;
+	}
+	return 0;
+}
