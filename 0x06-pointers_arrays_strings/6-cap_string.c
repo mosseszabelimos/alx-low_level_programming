4bf9772ab78964f3f4f8f8aef9fa549fd1f420a3
Abel <abeldawit.h@gmail.com>
2022-12-22 05:10:48 -0800

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
+	
 		if (str[index - 1] == ' ' ||
 		   str[index - 1] == '\t' ||
 		   str[index - 1] == '\n' ||
@@ -27,11 +28,12 @@ char *cap_string(char *str)
 		   str[index - 1] == ')' ||
 		   str[index - 1] == '{' ||
 		   str[index - 1] == '}' ||
-		   
 		   index == 0)
 		   	str[index] -= 32;
+
 		index++;
 	}
+
 	return (str);
 }
 
