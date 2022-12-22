6d5eab7a011f1823ebc6f23f6b675850a9ac57bd
Abel <abeldawit.h@gmail.com>
2022-12-22 07:15:13 -0800

    9. Numbers have life

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef main_h
 #define main_h
-
+int _putchar(char c);
 char *_strcat(char *ch1, char *ch2);
 char *_strncat(char *ch1, char *ch2, int num);
 char *_strncpy(char *ch1, char *ch2, int num);
