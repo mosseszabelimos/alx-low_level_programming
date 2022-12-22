eec7734eda9d96f52854e6609a0663f1f364e270
Abel <abeldawit.h@gmail.com>
2022-12-22 04:58:00 -0800

    1.strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,20 @@
 char *_strncat(char *ch1, char *ch2, int num)
 {
 	int x;
-	int y = 0;
+	int y;
 
 	x = 0;
-	
-	while (ch1[x++])
-		y++;
-
-	for (x = 0; ch2[x] && x < num; x++)
-		ch1[y] = ch2[x];
-	
+	while (ch1[x] != '\0')
+	{
+		x++;
+	}
+	y = 0;
+	while (y < num && ch2[y] != '\0')
+	{
+	ch1[x] = ch2[y];
+	x++;
+	y++;
+	}
+	ch1[x] = '\0';
 	return (ch1);
 }
