d1218733f96a59f1d6151345c2427f5d9a5e5285
Abel <abeldawit.h@gmail.com>
2022-12-22 06:11:47 -0800

    8.rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,43 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rot13 – Encodes using rot13
- *
- * Return: A pointer to the encoded ch1in
+ * rot13 - ...
+ * @s: ...
+ * Return: ...
  */
 
-char *rot13(char *ch1)
+char *rot13(char *s)
 {
-	int i= 0, j;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-			     'G', 'H', 'I', 'I', 'K', 'L',
-			     'M', 'N', 'O', 'P', 'Q', 'R',
-			     'S', 'T', 'U', 'V', 'W', 'X',
-			     'Y', 'Z', 'a', 'b', 'c', 'd',
-			     'e', 'f', 'g', 'h', 'i', 'i',
-			     'k', 'l', 'm', 'n', 'o', 'p',
-			     'q', 'r', 's', 't', 'u', 'v',
-			     'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-			     'T', 'U', 'V', 'W', 'X', 'Y',
-			     'Z', 'A', 'B', 'C', 'D', 'E',
-			     'F', 'G', 'H', 'I', 'I', 'K',
-			     'L', 'M', 'n', 'o', 'p', 'q', 
-			     'r', 's', 't', 'u', 'v', 'w',
-			     'x', 'y', 'z', 'a', 'b', 'c',
-			     'd', 'e', 'f', 'g', 'h', 'i',
-			     'i', 'k', 'l', 'm'};
-	while (ch1[i])
+	int a = 0;
+	
+	while (s[a])
 	{
-		for (j = 0; j < 52; j++)
-		{ 
-			if (ch1[i] == alphabet[j])
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
 			{
-				ch1[i] = rot13key[j];
+				s[a] -= 13;
 				break;
 			}
+			s[a] += 13;
+			break;
 		}
-		i++;
+		a++;
 	}
-	return (ch1);
+		return (s);
 }
--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -17,4 +17,5 @@ int main(void)
 				        _putchar('\n');
 					    print_number(-98);
 					        _putchar('\n');
-						    return (0)
+						    return (0);
+}
