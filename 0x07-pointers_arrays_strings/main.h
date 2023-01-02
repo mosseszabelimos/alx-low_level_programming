14ed096ec0d63713c27af82af833cb9aa33042b2
Abel <abeldawit.h@gmail.com>
2023-01-02 05:39:36 -0800

    4. strpbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ char *_memset(char *x, char ch, unsigned int n);
 char *_memcpy(char *x, char *y, unsigned int num);
 char *_strchr(char *ch1, char ch2);
 unsigned int _strspn(char *ch1, char *ch2);
+char *_strpbrk(char *ch1, char *ch2);
 #endif
