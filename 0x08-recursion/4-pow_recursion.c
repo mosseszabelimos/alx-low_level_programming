20e5d14083a83db539874988c95a8c960bbfc32f
Abel <abeldawit.h@gmail.com>
2023-01-04 04:06:06 -0800

    4. Once an idea has taken hold of the brain its almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,7 +15,6 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	else
-		pow = pow * _pow_recursion(x, y - 1);
+	pow = pow * _pow_recursion(x, y - 1);
 	return (pow);
 }
