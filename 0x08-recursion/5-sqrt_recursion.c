2501845a8f3ca660786c60e194aa00c0e7b058bb
Abel <abeldawit.h@gmail.com>
2023-01-04 04:35:49 -0800

    Your subconsious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,18 +11,24 @@ int _sqrt_recursion(int n)
 	int sq = 0;
 
 	if (n < 0)
-	       return (-1);
+		return (-1);
 	if (n == 1)
 		return (1);
 	return (sqr(n, sq));
 }
+/**
+ * sqr - gives the square root result
+ * @n: first entry
+ * @sq: second entry
+ * Return: value 0 or 1
+ */
 
 int sqr(int n, int sq)
 {
 	if ((sq * sq) == n)
 		return (sq);
-	if (sq == n/2)
+	if (sq == n / 2)
 		return (-1);
-	return (sqr(n, sq+1));
+	return (sqr(n, sq + 1));
 }
 
