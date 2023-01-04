46392b11715b8634a9d6cd36dcd8f226a81e4c50
Abel <abeldawit.h@gmail.com>
2023-01-04 03:33:06 -0800

    2. Dreams feel real while we aare in them.

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - tells the legth of a string
+ * @s: is the string
+ * Return: value 1 or 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	int str_len = 0;
+
+	if(*s)
+	{
+		str_len = str_len + _strlen_recursion(s + 1);
+		str_len++;
+	}
+	return (str_len);
+}
+		
