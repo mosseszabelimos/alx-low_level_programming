6b3ec249c6cff530d14ba50cbde0c98d59faab46
Abel <abeldawit.h@gmail.com>
2023-01-04 03:45:51 -0800

    3. You mustnt be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - gives a factorial of a given number
+ * @n: in entry number
+ * Retunr: value 1 or 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n <= 1)
+		return 1;
+	else
+		return n * factorial(n -1);
+}
