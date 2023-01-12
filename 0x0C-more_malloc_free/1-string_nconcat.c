e25ac828517c2e140e042ddf251640e4455c07d5
Abel <abeldawit.h@gmail.com>
2023-01-12 05:38:37 -0800

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *z;
 	unsigned int x = 0, y = 0, num1 = 0, num2 = 0;
 
-	while (s1 && s1[num11])
+	while (s1 && s1[num1])
 		num1++;
-	while (s2 && s2[num22])
+	while (s2 && s2[num2])
 		num2++;
 	if (n < num2)
 		z = malloc(sizeof(char) * (num1 + n + 1));
@@ -27,11 +27,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	while (x < num1)
 	{
-		s[x] = s1[x];
+		z[x] = s1[x];
 		x++;
 	}		
 	while (n < num2 && x < (num1 + n))
-		z[i++] = s2[j++];
+		z[x++] = s2[y++];
 	while (n >= num2 && x < (num1 + num2))
 		z[x++] = s2[y++];
 	z[x] = '\0';
