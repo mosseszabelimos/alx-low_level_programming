b851a5c9a9efda9f767fd39946cb7d95ab62964b
Abel <abeldawit.h@gmail.com>
2023-01-27 06:44:19 +0300

    print f assignement update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,11 +10,11 @@
  * @next: pointer
  */
 
-typedef struct lists_s
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct lists_s *next;
+	struct list_s *next;
 }list_t;
 
 size_t print_list(const list_t *h);
