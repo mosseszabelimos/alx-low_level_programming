25120be28d32c09ebb921fc1550aba633e310184
Abel <abeldawit.h@gmail.com>
2023-02-01 10:14:39 -0800

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,11 +2,10 @@
 #include "lists.h"
 
 /**
-  * print_listint - prints all the elements
-  * @h: The head of the linked list
-  *
-  * Return: The number of nodes of the linked list
-  */
+ * print_listint - prints all the elements
+ * @h: The head of the linked list
+ * Return: The number of nodes of the linked list
+ */
 
 size_t print_listint(const listint_t *h)
 {
